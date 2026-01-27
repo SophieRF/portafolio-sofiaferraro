b98cd84d2a3a67b81df6e966601985105607fe07
SophieRF <sofiaferraro2110@gmail.com>
2026-01-27 10:26:14 -0300

    mini updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import { LoadingScreen } from './components/LoadingScreen'
 import "./index.css"
 import { NavBar } from './components/NavBar';
 import { MobileMenu } from './components/MobileMenu';
@@ -10,23 +9,16 @@ import { Projects } from './components/sections/Projects';
 import { Contact } from './components/sections/contact/Contact';
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
-    {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)} />}{" "}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"
-      } bg-[#f3f3d1] text-gray-100`}>
-
         <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <Home />
         <About />
         <Projects />
         <Contact />
-
-      </div>
     </>
   )
 }
